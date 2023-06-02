a89c2d5f8d55159f005362ca80ac2a943f0c6620
RoboXGamer <roboxgamer52@gmail.com>
2023-06-02 13:22:14 +0530

    Fix Some Inconsistent Styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 // import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./NavBar.css";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const dropdownItemsRef = useRef([]);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -12,7 +15,6 @@ export default function NavBar() {
   const closeDropdown = () => {
     setIsOpen(false);
   };
-  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -21,14 +23,53 @@ export default function NavBar() {
   const closeMenu = () => {
     setMenuOpen(false);
   };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (!isOpen) return;
+
+    const { key } = event;
+
+    if (key === "ArrowUp" || key === "ArrowDown") {
+      event.preventDefault();
+
+      const currentIndex = dropdownItemsRef.current.findIndex(
+        (item) => item === document.activeElement
+      );
+
+      if (key === "ArrowUp") {
+        const previousIndex =
+          currentIndex > 0
+            ? currentIndex - 1
+            : dropdownItemsRef.current.length - 1;
+        dropdownItemsRef.current[previousIndex].focus();
+      } else if (key === "ArrowDown") {
+        const nextIndex =
+          currentIndex < dropdownItemsRef.current.length - 1
+            ? currentIndex + 1
+            : 0;
+        dropdownItemsRef.current[nextIndex].focus();
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
       <nav className="navbar">
         <h1>BBL Website</h1>
-        <div
-          className="navbar-menu"
-          style={({ display: "inherit" }, { alignItems: "center" })}
-        >
+        <div className="navbar-menu">
           <button className="menu-toggle" onClick={toggleMenu}>
             {isMenuOpen ? "Close" : "Menu"}
           </button>
@@ -43,54 +84,93 @@ export default function NavBar() {
                 About
               </a>
             </li>
-            <div className="dropdown">
+            <div className="dropdown" ref={dropdownRef}>
               <button className="dropdown-toggle" onClick={toggleDropdown}>
                 Modpacks
               </button>
               {isOpen && (
-                <ul className="dropdown-menu slide-fade">
+                <ul
+                  className="dropdown-menu slide-fade"
+                  onKeyDown={handleKeyDown}
+                >
                   <li>
-                    <a href="#option1" onClick={closeDropdown}>
+                    <a
+                      href="#option1"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[0] = el)}
+                    >
                       Technopolis
                     </a>
                   </li>
                   <li>
-                    <a href="#option2" onClick={closeDropdown}>
+                    <a
+                      href="#option2"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[1] = el)}
+                    >
                       Technopolis 2
                     </a>
                   </li>
                   <li>
-                    <a href="#option3" onClick={closeDropdown}>
+                    <a
+                      href="#option3"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[2] = el)}
+                    >
                       Seaopolis
                     </a>
                   </li>
                   <li>
-                    <a href="#option4" onClick={closeDropdown}>
+                    <a
+                      href="#option4"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[3] = el)}
+                    >
                       Seaopolis 2
                     </a>
                   </li>
                   <li>
-                    <a href="#option5" onClick={closeDropdown}>
+                    <a
+                      href="#option5"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[4] = el)}
+                    >
                       Caveopolis
                     </a>
                   </li>
                   <li>
-                    <a href="#option6" onClick={closeDropdown}>
+                    <a
+                      href="#option6"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[5] = el)}
+                    >
                       Infernopolis
                     </a>
                   </li>
                   <li>
-                    <a href="#option7" onClick={closeDropdown}>
+                    <a
+                      href="#option7"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[6] = el)}
+                    >
                       Skyopolis 3
                     </a>
                   </li>
                   <li>
-                    <a href="#option8" onClick={closeDropdown}>
+                    <a
+                      href="#option8"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[7] = el)}
+                    >
                       Skyopolis 4
                     </a>
                   </li>
                   <li>
-                    <a href="#option9" onClick={closeDropdown}>
+                    <a
+                      href="#option9"
+                      onClick={closeDropdown}
+                      ref={(el) => (dropdownItemsRef.current[8] = el)}
+                    >
                       Skyopolis 5
                     </a>
                   </li>
